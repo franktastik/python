474c944aabc271303771ca34e6d9be360c17688a
Franklin <franktastik@users.noreply.github.com>
2022-02-01 13:20:30 +0200

    Memmetrics

--- memmetrics.py
+++ memmetrics.py
@@ -1,7 +1,7 @@
 import psutil
 from psutil._common import bytes2human
 
-
+# Function to store and order the result as an integer data type
 def pprint_ntuple(nt):
     for name in nt._fields:
         value = getattr(nt, name)
@@ -9,7 +9,7 @@ def pprint_ntuple(nt):
             value = int(value)
         print('%-10s : %7s' % (name.capitalize(), value))
 
-
+# Function to print the result of memory metrics
 def main():
     print('\nMEMORY\n------')
     print('\nVirtual memory\n--------------')
